749929a1a711dfd586709a87a1d34d5361e74272
Sane <matias.moseley@gmail.com>
2023-03-21 14:26:31 -0700

    Refactored paramter checking, and created pthreads

--- scheduler.c
+++ scheduler.c
@@ -1,36 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
-void errorWithMessage (char *message){
+#define THREADS 3
+
+void errorWithMessage (char *message){ // Output error message and exit program with failure
     printf("%s\n", message);
     exit(1);
 }
 
-void main (int argc, char *argv[]){
+void readThread(void *arg){ // Thread function for reading from the input file
+    printf("I will read from input file\n");
+    pthread_exit(NULL);
+}
 
-    if (argc < 5 || argc > 7) errorWithMessage("Invalid parameters");
+void cpuThread(void *arg){ // Thread function for simulating CPU bursts based on scheduling algorithm
+    printf("I will manage Ready Queue\n");
+    pthread_exit(NULL);
+}
+
+void ioThread(void * arg){ // Thread function for simulating IO bursts in FIFO order
+    printf("I will manage IO Queue\n");
+    pthread_exit(NULL);
+}
+
+void cleanUpThreads(pthread_t *threads){ // Clean up threads
+    for (int i = 0; i < THREADS; i++) pthread_join(threads[i], NULL);
+}
+
+void main (int argc, char *argv[]){
+    char *algos[] = {"FCFS", "SJF", "PR", "RR"};
+    int valid = 0;
+    int quantum = 0;
+
+    if (argc < 5 || argc > 7) errorWithMessage("Invalid parameters"); // Check for valid parameters
     if (strcmp(argv[1], "-alg")) errorWithMessage("Missing algorithm");
     if (strcmp(argv[argc - 2], "-input")) errorWithMessage("Missing input file");
 
-    char *algo = argv[2];
-    char *file = argv[argc - 1];
+    for (int i = 0; i < 4; i++) { // Check for valid algorithm
+        if (!strcmp(argv[2], algos[i])) {
+            if (i == 4 && (strcmp(argv[3], "-quantum") || (quantum = atoi(argv[4])) < 1)) errorWithMessage("Missing or invalid quantum");
+            valid = 1;
+            break;
+        }
+    } if (!valid) errorWithMessage("Invalid algorithm");
 
-    if (!strcmp(algo, "FCFS")){ // First Come First Serve
-        printf("First Come First Serve\n");
-    }
-    else if (!strcmp(algo, "SJF")){ // Shortest Job First
-        printf("Shortest Job First\n");
-    }
-    else if (!strcmp(algo, "PR")){ // Priority Queue
-        printf("Priority Queue\n");
-    }
-    else if (!strcmp(algo, "RR")){ // Round Robin
-        int quantum;
-        if (strcmp(argv[3], "-quantum") || (quantum = atoi(argv[4])) < 1) errorWithMessage("Missing or invalid quantum");
-        printf("Round Robin: Quantum = %d\n", quantum);
-    }
-    else errorWithMessage("Invalid algorithm");
+    pthread_t threads[THREADS];
+    void *threadFunctions[THREADS] = {readThread, cpuThread, ioThread};
+    void *threadArgs[THREADS] = {argv[argc - 1], argv[2], NULL};
 
+    for (int i = 0; i < THREADS; i++){ // Create pthreads
+        if (pthread_create(&threads[i], NULL, (void *)threadFunctions[i], threadArgs[i])){
+            cleanUpThreads(threads);
+            errorWithMessage("Thread creation failed");
+        }
+    }
+
+    cleanUpThreads(threads);
     exit(0);
 }
